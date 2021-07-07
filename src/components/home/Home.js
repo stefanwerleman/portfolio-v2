bdc52cb24d3dceeaf6056416134f77379973b2d9
Stefan Werleman <stefanwerleman@knights.ucf.edu>
2021-07-07 16:52:32 -0400

    Front page designs finished

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,9 +6,13 @@ import Img from './self_image.png';
 function Home() {
 	return (
 		<div className='Home'>
-			{/* <div className='FrontHeader'> */}
-			{/* <img src={Img} alt='self_image' /> */}
-			{/* </div> */}
+			<div className='FrontHeader'>
+				<img src={Img} alt='self_image' />
+				<div className='FrontBody'>
+					<p className='HeaderName'>Stefan Werleman</p>
+					<p className='SubName'>Full Stack Developer</p>
+				</div>
+			</div>
 		</div>
 	);
 }
