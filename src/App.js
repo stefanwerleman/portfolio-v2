89973671960065d47ed48479ef27008d0714c4ee
Stefan Werleman <stefanwerleman@knights.ucf.edu>
2021-07-08 22:10:56 -0400

    Starting projects

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Navigation from './components/navigation/Navigation';
 import Home from './components/home/Home';
 import About from './components/about/About';
-// import Projects from './components/projects/Projects';
+import Projects from './components/projects/Projects';
 import Skills from './components/skills/Skills';
 import Education from './components/education/Education';
 import Contact from './components/contact/Contact';
@@ -19,6 +19,7 @@ function App() {
 				<Navigation />
 				<Home />
 				<About />
+				<Projects />
 				<Skills />
 				<Education />
 				<Contact />
