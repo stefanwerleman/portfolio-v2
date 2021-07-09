76138e78211a63daaa3cef368820101e33395549
Stefan Werleman <stefanwerleman@knights.ucf.edu>
2021-07-09 10:38:45 -0400

    added sections

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,11 +1,58 @@
 import './Projects.css';
 import React from 'react';
 
+// Child Component
+import Project from '../project/Project';
+
+// Assets 
+import { websites, mobiles, algorithms, desktop, others } from '../utils/utils';
+
+
 function Projects() {
+	const [section, setSection] = React.useState(0);
+
+	const websiteSection = () => {
+		return (<div className='SubSection'><p className='SubSectionHeader' >Websites</p></div>)
+	}
+
+	const mobileSection = () => {
+                return (<div className='SubSection'><p className='SubSectionHeader' >Mobile Applications</p></div>)
+        }
+
+	const algorithmsSection = () => {
+                return (<div className='SubSection'><p className='SubSectionHeader' >Algorithms and Data Structures</p></div>)
+        }
+
+	const desktopSection = () => {
+                return (<div className='SubSection'><p className='SubSectionHeader' >Desktop Applications</p></div>)
+        }
+
+	const otherSection = () => {
+                return (<div className='SubSection'><p className='SubSectionHeader' >Other Projects</p></div>)
+        }
+
+
+	const toggleSection = () => {
+		if (section === 0) {} 
+		else if (section === 1) {}
+		else if (section === 2) {}
+		else if (section === 3) {}
+		else if (section === 4) {}
+		else {}
+	};
+
 	return (
 		<div className='Projects' id='projects'>
-			<p className='SectionHeader'>Projects</p>
-			<h2 style={{ color: '#ed5565' }}>Projects are coming soon!</h2>
+			<div className='ProjectsBody'>
+				<p className='SectionHeader'>Projects</p>
+
+				{websiteSection()}
+				{mobileSection()}
+				{algorithmsSection()}
+				{desktopSection()}
+				{otherSection()}
+				{websites.map((website) => (<Project name={website.name} url={website.url} github={website.github} description={website.description} tools={website.tools} image={website.image}/>))}	
+			</div>
 		</div>
 	);
 }
