f4bc00de5a59873a298e9abb6c440b8b8618eeba
Stefan Werleman <stefanwerleman@knights.ucf.edu>
2021-07-08 23:04:38 -0400

    Fixed padding

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -5,6 +5,7 @@ function Projects() {
 	return (
 		<div className='Projects' id='projects'>
 			<p className='SectionHeader'>Projects</p>
+			<h2 style={{ color: '#ed5565' }}>Projects are coming soon!</h2>
 		</div>
 	);
 }
