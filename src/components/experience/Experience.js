1e0108d9fc9d8ae85b43beb98cbe909b1e50d1d7
stefanwerleman <stefanwerleman@knights.ucf.edu>
2022-02-03 21:58:52 -0500

    Finished update

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,7 +1,8 @@
 import './Experience.css';
 import React from 'react';
 
-import Img from './siemens.png';
+import Siemens from './siemens.png';
+import Qorvo from './qorvo.jpeg';
 
 function Experience() {
 	return (
@@ -13,17 +14,35 @@ function Experience() {
 			<div className='ExperienceBody'>
 				<p className='SectionHeader'>Experience</p>
 				<div className='Company'>
-					<img src={Img} alt='siemens-logo' />
+					<img src={Qorvo} alt='qorvo-logo' />
+					<div className='CompanyBody'>
+						<p className='CompanyName'>
+							Software Engineer Intern, EDA Automation
+						</p>
+						<div className='CompanyInfo'>
+							<p>Qorvo</p>
+							<p>February 2022 - Present</p>
+						</div>
+						{/* <p className='Description'>
+							Developing a job listing platform exclusively for
+							job seekers and employers within the energy
+							industry.
+						</p> */}
+					</div>
+				</div>
+				<div className='Company'>
+					<img src={Siemens} alt='siemens-logo' />
 					<div className='CompanyBody'>
 						<p className='CompanyName'>Software Engineer Intern</p>
 						<div className='CompanyInfo'>
 							<p>Siemens</p>
-							<p>August 2021 - Present</p>
+							<p>August 2021 - February</p>
 						</div>
 						<p className='Description'>
-							Developing a job listing platform exclusively for
-							job seekers and employers within the energy
-							industry.
+							Developed a job listing platform exclusively for job
+							seekers and employers within the energy industry.
+							Met with talent acquisition executives to promote
+							and demo the prototype.
 						</p>
 					</div>
 				</div>
